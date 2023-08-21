1695fa9836b1fca45a854ae335e4bc3fb5ef980c
Matías <69920714+MatiasVilla91@users.noreply.github.com>
2023-08-21 18:31:10 -0300

    4toSemestre_clase1.py

--- Python/Agosto/Semana-14-al-18.py
+++ Python/Agosto/Semana-14-al-18.py
@@ -46,3 +46,49 @@ class UsuarioDAO:
             return cursor.rowcount
 
 #Fin ejercicio Gerardo Duckwitz
+
+
+#Ejercicios Matías Villa
+
+from capa_datos_personas.Usuario import Usuario
+from capa_datos_personas.usuario_dao import UsuarioDAO
+from logger_base import log
+
+
+opcion = None
+while opcion != 5:
+    print('Opciones: ')
+    print('1.Listar Usuarios')
+    print('2.Agregar Usuarios')
+    print('3.Modificar usuarios')
+    print('4.Eliminar usuario')
+    print('5.Salir')
+    opcion= int(input('Digite una opción (1-5): '))
+    if opcion == 1 :
+        usuarios = UsuarioDAO.seleccionar() #desde personaDAO se añade listar
+        for usuario in usuarios:
+            log.info(usuario)
+
+    elif opcion == 2:
+        username_var = input('Digite el nombre de usuario: ')
+        password_var = input('Digite contraseña: ')
+        usuario = Usuario(username=username_var, password=password_var)
+        usuario_insertado = UsuarioDAO.insertar(usuario)
+        log.info(f'Usuario insertado: {usuario_insertado}')
+
+    elif opcion == 3:
+        id_usuario_var = int(input('Digite id de usuario a modificar: '))
+        username_var = input('Digite el nombre del usuario a modificar: ')
+        password_var = input('Digite la contraseña del usuario a modificar: ')
+        usuario = Usuario(id_usuario=id_usuario_var, username=username_var, password=password_var)
+        usuario_actualizado = UsuarioDAO.actualizar(usuario)
+        log.info(f'Usuario actualizado: {usuario_actualizado}')
+    elif opcion == 4:
+        id_usuario_var= int(input('Digite el usuario  a eliminar'))
+        usuario = Usuario(id_usuario=id_usuario_var)
+        usuario_eliminado= UsuarioDAO.eliminar(usuario)
+        log.info(f'Usuario eliminado: {usuario_eliminado}')
+else:
+    log.info('Salimos de la Aplicación, Hasta pronto!')
+
+#Fin Ejercicio Matías
