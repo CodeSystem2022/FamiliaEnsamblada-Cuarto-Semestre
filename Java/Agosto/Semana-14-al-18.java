d9a84304c6a9ab48ca2b04a3123f54dfcc992bc6
alesciAnabel <anabelalesci24@gmail.com>
2023-08-22 21:34:12 -0300

    subo ejercicio java semana 14 al 18

--- Java/Agosto/Semana-14-al-18.java
+++ Java/Agosto/Semana-14-al-18.java
@@ -52,6 +52,7 @@
     }
 
 //Fin ejercicio Gerardo Duckwitz
+*
 
 //Ejercicio Jose Remaggi
   public boolean eliminarEstudiante(Estudiate estudiante){
@@ -107,3 +108,90 @@
     }
 
 //Fin ejercicio Jose Remaggi
+//Ejercicio Anabel Alesci
+//Metodo para ejecutar las opciones, va a regresar un valor booleani, ya que es el que
+// puede modificar el valor de la variable salir, si es verdadero termina el ciclo while
+private static boolean ejecutarOpciones(Scanner consola, EstudianteDAO estudianteDAO){
+  var opcion = Integer.parseInt(consola.nextLine());
+  var salir = false;
+  switch(opcion){
+    case 1 -> {//listar estudiantes
+      System.out.println("Listado de Estudiantes");
+      //no muestra la informacion, solo recupera la info y regresa una lista
+        var estudiantes = estudianteDAO.listarEstudiantes();//recibe el listado
+        //vamos a iterar cada objeto de tipo estudiante
+        estudiantes.forEach(System.out::println);//para imprimir la lista
+        }//Fin Caso 1
+        case 2 -> {//Buscar estudiante por id
+      System.out.println("Introduce el id_estudiante a buscar: ");
+      var idEstudiante = Integer.parseInt(consola.nextLine());
+      var estudiante = new Estudiante(idEstudiante);
+      var encontrado = estudianteDAO.buscarEstudiantePorId(estudiante);
+      if(encontrado)
+        System.out.println("Estudiante encontrado: "+ estudiante);
+      else
+        System.out.prinln("Estudiante NO encontrado: "+ estudiante);
+        }//Fin caso 2
+        case 3 ->{//Agregar estudiante
+        System.out.println("Agregar estudiante: ");
+        System.out.println("Nombre: ");
+        var nombre=console.nextLine();
+        System.out.println("Apellido: ");
+        var apellido=console.nextLine();
+        System.out.println("Telefono: ");
+        var telefono=console.nextLine();
+        System.out.println("Email: ");
+        var email=console.nextLine();
+        //crear objeto estudiante(sin id)
+        var estudiante=new Estudiante(nombre,apellido,telefono,email);
+        var agregado=estudianteDAO.agregarEstudiante(estudiante);
+        if(agregado)
+        System.out.println("Estudiante agregado: "+estudiante);
+        else
+        System.out.println("Estudiante NO agregado"+estudiante);
+        }//Fin Caso 3
+        case 4 -> {//Modificar estudiante
+      System.out.println("Modificar estudiante: ");
+      //Aqui lo primero es especificar cual es el id del objeto a modificar
+        System.out.println("Id Estudiante: ");
+        var idEstudiante =Integer.parseInt(consola.nextLine());
+        System.out.println("Nombre: ");
+        var nombre = consola.nextLine();
+        System.out.println("Apellido: ");
+        var apellido = consola.nextLine();
+        System.out.println("Telefono: ");
+        var telefono = consola.nextLine();
+        System.out.println("Email: ");
+        var email = consola.nextLine();
+        //crea el objeto estudiante a modificar
+        var estudiante =
+        new Estudiante(idEstudiante, nombre, apellido, telefono, email);
+        var modificado = estudianteDAO.modificarEstudiante(estudiante);
+        if(modificado)
+        System.out.println("Estudiante modificado"+ estudiante);
+        else
+          System.out.println("Estudiante No modificado"+ estudiante);
+    }//Fin caso 4
+        case 5 -> {//Eliminar estudiante
+        System.out.println("Eliminar estudiante: ");
+        System.out.println("Id estudiante: ");
+        var idEstudiante = Integer.parseInt(consola.nextLine());
+        var estudiante = new Estudiante(idEstudiante);
+        var eliminado = estudianteDAO.eliminarEstudiante(estudiante);
+        if(eliminado)
+          System.out.println("Estudiante eliminado: "+ estudiante);
+        else
+          System.out.println("Estudiante NO eliminado: "+ estudiante);
+        }//Fin caso 5
+        case 6 ->{//salir
+        System.out.println("Hasta pronto!!!");
+        salir=true;
+        }//fin caso 6
+        default -> System.out.println("Opción no reconocida, ingrese otra opción");
+        }//Fin switch
+        return salir;
+        }
+        }//fin clase
+
+        }
+        //Fin Ejercicio Anabel Alesci
