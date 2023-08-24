290b1f6dc13b6340916a5032fd89d025ce25482a
gfduck <66056030+gfduck@users.noreply.github.com>
2023-08-24 19:54:18 -0300

    Gerardo Java 21 al 25

--- Java/Agosto/Semana-21-al-25.java
+++ Java/Agosto/Semana-21-al-25.java
@@ -1,1 +1,30 @@
+//Ejercicio Gerardo Duckwitz
+package utn.estudiantes;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudiantesApplication.class, args);
+	}
+
+}
+
+
+#Conexion MySQL
+spring.datasource.url=jdbc:mysql://localhost:3306/estudiantes
+spring.datasource.username=root
+
+spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+
+#Evitar que se cree el esquema nuevamente
+spring.jpa.hibernate.ddl-auto=none
+spring.jpa.show-sql=true
+
+#Desactivar el tomcat, esto es porque no es aplicacion web, es por terminal
+spring.main.web-application-type=none
+
+  //Fin ejercicio Gerardo Duckwitz
