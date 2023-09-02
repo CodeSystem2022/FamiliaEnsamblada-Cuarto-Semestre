f798bb00a9b662fb4d5928987a47aab7f41532c0
gfduck <66056030+gfduck@users.noreply.github.com>
2023-09-02 20:46:03 -0300

    Gerardo | Node | Semana 28 al 1

--- Node/Agosto/Semana-28-al-01.js
+++ Node/Agosto/Semana-28-al-01.js
@@ -1,1 +1,24 @@
+// Ejercicio Gerardo Duckwitz
+const saludar = (nombre) => {
+  //Función flecha
+  return `
+    Saludos de ${nombre}
+    `; // Usamos back tic`s
+}; //Esto es igual a 'Saludos'+ nombre;
+console.log(saludar("Ariel"));
 
+console.log("Inicio del programa");
+setTimeout(() => {
+  console.log("Primer Timeout");
+}, 3000);
+setTimeout(() => {
+  console.log("Segundo Timeout");
+}, 0);
+setTimeout(() => {
+  console.log("Tercero Timeout");
+}, 0);
+
+console.log("Fin del programa");
+
+
+//Fin ejercicio Gerardo Duckwitz
