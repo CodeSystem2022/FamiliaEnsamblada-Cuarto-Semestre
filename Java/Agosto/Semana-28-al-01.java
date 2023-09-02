c8f8e01d2ffad8ab70e00848c32758b93cb9a3d6
gfduck <66056030+gfduck@users.noreply.github.com>
2023-09-02 20:34:59 -0300

    Gerardo Java 28 al 1

--- Java/Agosto/Semana-28-al-01.java
+++ Java/Agosto/Semana-28-al-01.java
@@ -1,1 +1,28 @@
+// Ejericio Gerardo Duckwitz
 
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+//boilerplate - Repetitivo
+@Data
+ @NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Estudiante {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idestudiantes;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+
+}
+
+//Fin ejercicio Gerardo Duckwitz
