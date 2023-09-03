b33fc457ca258c22a0dfc4ac363008b22f60d4b0
Yesica López <112655115+Yesica-B-Lopez@users.noreply.github.com>
2023-09-02 22:41:18 -0300

    Yesica Semana-28-al-01.js

--- Node/Agosto/Semana-28-al-01.js
+++ Node/Agosto/Semana-28-al-01.js
@@ -22,3 +22,30 @@ console.log("Fin del programa");
 
 
 //Fin ejercicio Gerardo Duckwitz
+
+//Ejercicio de Yesica López
+
+//app2.js
+const saludar = (nombre) => { //Función flecha
+  return 'Saludos de ${nombre}'; // Usamos back tic`s};
+//esto es igual a: 'Saludos'+ nombre;
+console.log(saludar('Yesica'));
+
+//app3.js
+console.log("Inicio del programa"); //1
+
+setTimeout(() => {
+  console.log("Primer Timeout"); //5
+}, 3000);
+
+setTimeout(() => {
+  console.log("Segundo Timeout"); //3
+}, 0);
+
+setTimeout(() => {
+  console.log("Tercero Timeout"); //4
+}, 0);
+
+console.log("Fin del programa"); //2
+
+//Fin Ejercicio de Yesica López
