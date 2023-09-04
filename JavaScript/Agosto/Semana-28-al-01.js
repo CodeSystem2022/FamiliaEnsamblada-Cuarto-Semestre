77711c850b2e8dbfcbe0045893ff69e3f346fcd3
Jose Remaggi <joseluisremaggi@gmail.com>
2023-09-04 20:22:08 -0300

    ejercicios jose remaggi agosto

--- JavaScript/Agosto/Semana-28-al-01.js
+++ JavaScript/Agosto/Semana-28-al-01.js
@@ -112,3 +112,40 @@ const displayCart = () => {
 cartBtn.addEventListener("click", displayCart);
 
 //Fin Ejercicio Yesica López
+
+//Ejercicio de Jose Remaggi
+const modalContainer = document.getElementById("#modal-container");
+const modalOverlay = document.getElementById("#modal-overlay");
+
+const cartBtn = document.getElementById("cart-btn");
+
+const displayCart = () => {
+  modalContainer.innerHTML = "";
+  modalContainer.style.display = "block";
+  modalOverlay.style.display = "block";
+
+  //modal header
+  const modalHeader = document.createElement("div");
+ 
+  const modalClose = document.createElement("div");
+  modalClose.innerText = "✖️";
+  modalClose.className = "modal-close";
+  modalHeader.append(modalClose);
+
+  modalClose.addEventListener("click", () => {
+    modalContainer.style.display = "none";
+    modalOverlay.style.display = "none";
+  });
+
+  const modalTitle = document.createElement("div");
+  modalTitle.innerText = "Cart";
+  modalTitle.className = "modal-title";
+  modalHeader.append(modalTitle);
+
+  modalContainer.append(modalHeader);
+};
+
+cartBtn.addEventListener("click", displayCart);
+
+//Fin Ejercicio Jose Remaggi
+
