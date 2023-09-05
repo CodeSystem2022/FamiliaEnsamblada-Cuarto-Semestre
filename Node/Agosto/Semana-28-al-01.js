fe42cd79693894fd3d4885b7032bbb7e05ed9788
Matías <69920714+MatiasVilla91@users.noreply.github.com>
2023-09-05 02:19:03 -0300

    Update Semana-28-al-01.js

--- Node/Agosto/Semana-28-al-01.js
+++ Node/Agosto/Semana-28-al-01.js
@@ -99,3 +99,36 @@ setTimeout(() => {
 console.log("Fin del programa");
 
 // Fin ejercicio Anabel Alesci
+
+//Ejercicio Matías Villa
+
+  const saludar = (nombre) => {   //funcion de flecha
+    return `Saludos de ${nombre}` //ALT+96
+} //esto es igual a 'saludos'+nombre;
+
+console.log(saludar('Ariel'))
+
+
+
+
+  console.log('Inicio del programa');
+setTimeout(() => {
+    console.log('Primer Timeout');
+}, 3000); //se le agrega el tiempo
+
+console.log('Inicio del programa');
+setTimeout(() => {
+    console.log('Segundo Timeout');
+}, 0); //se le agrega el tiempo
+
+console.log('Inicio del programa');
+setTimeout(() => {
+    console.log('Tercer Timeout');
+}, 0); //se le agrega el tiempo
+
+console.log('Fin del programa   ');
+
+
+
+  
+  //Fin ejercicio Matías Villa
