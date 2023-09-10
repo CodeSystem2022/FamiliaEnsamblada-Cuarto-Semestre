e15966515f148b061396104be7b078ac288813f4
Jesús Mercado <83405417+mercadojesus@users.noreply.github.com>
2023-09-09 23:46:41 -0300

    Update Semana-21-al-25.java
    Ejercicios de Java correspondientes a la semana del 21/08 al 25/08

--- Java/Agosto/Semana-21-al-25.java
+++ Java/Agosto/Semana-21-al-25.java
@@ -181,4 +181,35 @@ spring.main.web-application-type=none
 #limpia la consola
 
 //Fin Ejercicio Matías Villa
+
+// Ejercicio Jesús Mercado
+
+package utn.estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudiantesApplication.class, args);
+	}
+
+}
+
+# Conexión MySQL
+spring.datasource.url=jdbc:mysql://localhost:3306/estudiantes
+spring.datasource.username=root
+spring.datasource.password=admin
+spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+
+# Evitar que se cree el esquema nuevamente
+spring.jpa.hibernate.ddl-auto=none
+spring.jpa.show-sql=true
+
+# Desactivar el tomcat, esto es porque no es aplicacion web, es por terminal
+spring.main.web-application-type=none
+	
+// Fin Ejercicio Jesús Mercado
 	
