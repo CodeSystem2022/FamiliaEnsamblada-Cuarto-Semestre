1c01d07e871ad3f228c648213fa249dab80f6771
Jose Remaggi <joseluisremaggi@gmail.com>
2023-09-12 22:53:01 -0300

    ejercicio semana 4 al 8 Jose Remaggi

--- Java/Septiembre/Semana-04-al-08.java
+++ Java/Septiembre/Semana-04-al-08.java
@@ -72,3 +72,78 @@ public class EstudiantesApplication implements CommandLineRunner {
 
 
 //Fin ejercicio Gerardo Duckwitz
+
+// Ejercicio Jose Remaggi
+package utn.estudiantes.servicio;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.repositorio.EstudianteRepositorio;
+
+import java.util.List;
+
+@Service
+public class EstudianteServicio implements IEstudianteServicio{
+    @Autowired
+    private EstudianteRepositorio estudianteRepositorio;
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
+      Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null);
+      return estudiante;
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.save(estudiante);
+        
+    }
+
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.delete(estudiante);
+    }
+}
+package utn.estudiantes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import utn.estudiantes.servicio.EstudianteServicio;
+
+@SpringBootApplication
+public class EstudiantesApplication implements CommandLineRunner {
+	@Autowired
+	private EstudianteServicio estudianteServicio;
+	private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+
+	String nl = System.lineSeparator();
+
+	public static void main(String[] args) {
+
+		logger.info("Iniciando la aplicacion...");
+		//Levantar la frabrica de Spring
+		SpringApplication.run(EstudiantesApplication.class, args);
+		logger.info("Aplicacion finalizada!");
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info(nl+"Ejecutando el metodo run de Spring..."+nl);
+
+	}
+}
+
+
+//Fin ejercicio Jose Remaggi
--- Python/Septiembre/Semana-04-al-08.py
+++ Python/Septiembre/Semana-04-al-08.py
@@ -189,3 +189,85 @@ else:
     print('Regresa falso')
 
 #Fin Ejercicio Yesica López
+
+# Ejercicio Jose Remaggi
+
+import math
+from decimal import Decimal
+
+# NaN(Not a Number)
+a = float('nan')
+print(f'a: {a}')
+
+# Módulo math
+a = float('nan')
+print(f'Es de tipo NaN(Not a Number)?: {math.isnan(a)}')
+
+# Módulo decimal
+a = Decimal('nan')
+print(f'Es de tipo NaN(Not a Number)?: {math.isnan(a)}')
+
+# Bool contiene los valores True y False
+# Los tipos numéricos, es false para el 0 (cero), true para los demás valores
+valor = 0
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+valor = 0.1
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+# Tipo string -> False '', True demás valores
+valor = ''
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+valor = 'Hola'
+resultado = bool(valor)
+print(f'valor: {valor}, Resultado: {resultado}')
+
+# Tipo colecciones -> False para colecciones vacias
+# Tipo colecciones -> True para todas las demás
+# lista
+valor = []
+resultado = bool(valor)
+print(f'valor de una lista vacía: {valor}, Resultado: {resultado}')
+
+valor = [2, 3, 4]
+resultado = bool(valor)
+print(f'valor de una lista con elementos: {valor}, Resultado: {resultado}')
+
+# Tupla
+valor = ()
+resultado = bool(valor)
+print(f'valor de una tupla vacía: {valor}, Resultado: {resultado}')
+
+valor = (5,)
+resultado = bool(valor)
+print(f'valor de una tupla con elementos: {valor}, Resultado: {resultado}')
+
+# Diccionario
+valor = {}
+resultado = bool(valor)
+print(f'valor de un diccionario vacío: {valor}, Resultado: {resultado}')
+
+valor = {'Nombre':'Jose', 'Apellido':'Remaggi'}
+resultado = bool(valor)
+print(f'valor de un diccionario con elementos: {valor}, Resultado: {resultado}')
+
+# Sentencias de control con bool
+if (1,):
+    print('Regresa verdadero')
+else:
+    print('Regresa falso')
+
+# Ciclos
+variable = 3
+while variable:
+    print('Regresa verdadero')
+    break
+else:
+    print('Regresa falso')
+
+  
+#Fin Ejercicio Jose Remaggi
