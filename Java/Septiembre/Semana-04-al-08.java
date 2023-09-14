26b7b50f017e6241cc49e9d4aecd5f5672ade1d3
Yesica López <112655115+Yesica-B-Lopez@users.noreply.github.com>
2023-09-13 21:33:13 -0300

    Yesica Semana-04-al-08.java

--- Java/Septiembre/Semana-04-al-08.java
+++ Java/Septiembre/Semana-04-al-08.java
@@ -297,3 +297,70 @@ public class EstudiantesApplication implements CommandLineRunner {
 
 
 // Fin Ejercicio Jesús Mercado
+
+//Ejercicio de Yesica López
+
+package utn.estudiantes.servicio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.repositorio.EstudianteRepositorio;
+import java.util.List;
+
+@Service
+public class EstudianteServicio implements IEstudianteServicio{
+    @Autowired
+    private EstudianteRepositorio estudianteRepositorio;
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+       return estudiantes;
+    }
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
+        Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null);
+       return estudiante;
+    }
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.save(estudiante);
+    }
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.delete(estudiante);
+    }
+}
+
+
+package utn.estudiantes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import utn.estudiantes.servicio.EstudianteServicio;
+
+@SpringBootApplication
+public class EstudiantesApplication implements CommandLineRunner {
+    @Autowired
+    private EstudianteServicio estudianteServicio;
+    private static final Logger logger = LoggerFactory.getLogger(EstudiantesApplication.class);
+
+    String nl = System.lineSeparator();
+
+public static void main(String[] args) {
+     logger.info("Iniciando la aplicación...");
+     //Levantar la fábrica de Spring
+     SpringApplication.run(EstudiantesApplication.class, args);
+     logger.info("Aplicación finalizada!");
+}
+     @Override
+     public void run(String... args) throws Exception {
+	logger.info(nl+"Ejecutando el método run de Spring..."+nl);
+     }
+} }
+
+//Fin Ejercicio Yesica López
