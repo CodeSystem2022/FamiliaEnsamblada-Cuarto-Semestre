661440a61f90abf886ae98d34b2ee89a423c2de1
Jesús Mercado <83405417+mercadojesus@users.noreply.github.com>
2023-09-15 12:37:09 -0300

    Update Semana-11-al-15.js
    Ejercicio Node correspondiente a la semana dal 11/09 al 15/09

--- Node/Septiembre/Semana-11-al-15.js
+++ Node/Septiembre/Semana-11-al-15.js
@@ -1,1 +1,16 @@
+// Ejercicio Jesús Mercado
+// monohilo.js
+console.log('Hola a toda la cohorte 2022');
 
+var i = 0;
+setInterval(function() {
+    console.log(i);
+    i++;
+
+    // if(i === 5){
+    //     console.log('Forzamos un error');
+    //     var a = 3 + z;
+    // }
+}, 1000);
+console.log('Segunda instrucción');
+// Fin Ejercicio Jesús Mercado
