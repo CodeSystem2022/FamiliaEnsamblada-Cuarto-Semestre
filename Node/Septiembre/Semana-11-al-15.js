68c582c7c53e74ac8954195cfe4d68ba86e96809
Matías <69920714+MatiasVilla91@users.noreply.github.com>
2023-09-18 02:36:24 -0300

    Update Semana-11-al-15.js

--- Node/Septiembre/Semana-11-al-15.js
+++ Node/Septiembre/Semana-11-al-15.js
@@ -66,3 +66,22 @@ setInterval(function() {
 }, 1000);
 console.log('Segunda instrucción');
 // Fin Ejercicio Anabel Alesci
+
+//Ejercicio Matías Villa
+
+console.log('Hola a todxs')
+
+var i = 0;
+setInterval(function(){
+    console.log(i);
+    i++;
+//    if (i==5){
+//        console.log('Forzamos un error');
+//       var a = 3 +z;
+
+
+}, 1000);
+console.log('Segun instrucción')
+
+//Ctr+C detiende el bucle
+//Fin ejercicio Matías Villa
