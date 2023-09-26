1c1263b1c6dfba746ea608e50abf0fc9c0ab66d1
Matías <69920714+MatiasVilla91@users.noreply.github.com>
2023-09-25 23:15:02 -0300

    Update Semana-11-al-15.js

--- JavaScript/Septiembre/Semana-11-al-15.js
+++ JavaScript/Septiembre/Semana-11-al-15.js
@@ -341,4 +341,90 @@ app.listen(8080, () => {
 
 //Fin Ejercicio Anabel Alesci
 
+//Ejercicio Matías Villa
+
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mercadopago = require("mercadopago");
+const path = require("path");
+
+//REPLACE WITH YOUR ACCESS TOKEN AVAILABLE IN: https://developers.mercadopago.com/panel
+mercadopago.configure({
+	access_token: "<ACCESS_TOKEN",
+});
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "../Client")));
+app.use(cors());
+
+// app.get("/", function() {
+// 	path.resolve(__dirname, "..", "Client", "media", "index.html");
+// });
+
+app.get("/", function(req, res) {
+	res.sendFile(path.resolve(__dirname, "..", "Client", "media", "index.html"));
+  });
+  
+
+app.post("/create_preference", (req, res) =>{
+	let preference = {
+		items: [{
+			title: req.body.description,
+			unit_price: Number(req.body.prince),
+			quantity: Number(req.body.quantity),
+		  },
+		],
+        back_urls: {
+        	success: "http://localhost:8080/",
+        	failure: "http://localhost:8080/",
+        	pending: "",
+        },
+        auto_return: "approved",
+       };
+	   mercadopago.preferences.create(preference)
+	   .then(function (response) {
+		   res.json({
+			   id: response.body.id
+		   });
+	   }).catch(function (error) {
+		   console.log(error);
+	   });
+});
+app.get("/feedback", function (req, res){
+	res.json({
+		Payment: req.query.payment_id,
+		Status: req.query.status,
+		MerchantOrder: req.query.merchant_order_id,
+	});
+});
+
+app.listen(8080, () => {
+	console.log("Corriendo el servidor en el puerto 8080");
+});
+
+//package.json
+
+{
+  "name": "server",
+  "version": "1.0.0",
+  "description": "",
+  "main": "server.js",
+  "scripts": {
+    "strat": "nodemon server.js",
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "author": "mati",
+  "license": "ISC",
+  "dependencies": {
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "mercadopago": "^1.5.17",
+    "nodemon": "^3.0.1"
+  }
+}
+
+//Fin enjercicio Matías
 
