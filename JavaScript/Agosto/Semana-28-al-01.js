f4a65bc0a930672a3c938b2bb9e02647ba309a76
VictoriaZaccaro <104931395+VictoriaZaccaro@users.noreply.github.com>
2023-09-29 01:08:24 -0300

    victoria zaccaro

--- JavaScript/Agosto/Semana-28-al-01.js
+++ JavaScript/Agosto/Semana-28-al-01.js
@@ -219,3 +219,36 @@ cartBtn.addEventListener("click", displayCart);
 
 // Fin Ejercicio Jesús Mercado
 
+//Ejercicio victoria zaccaro
+const modalContainer = document.getElementById("modal-container");
+const modalOverlay = document.getElementById("modal-overlay");
+
+const cartBtn = document.getElementById("cart-btn");
+const cartCounter = document.getElementById("cart-counter");
+
+const displayCart = () => {
+  modalContainer.innerHTML = "";
+  modalContainer.style.display = "block";
+  modalOverlay.style.display = "block";
+  
+ //modal header
+  const modalHeader = document.createElement("div");
+
+  const modalClose = document.createElement("div");
+  modalClose.innerText = "❌";
+  modalClose.className = "modal-close";
+  modalHeader.append(modalClose);
+
+  modalClose.addEventListener("click", () => {
+    modalContainer.style.display = "none";
+    modalOverlay.style.display = "none";
+  });
+
+  const modalTitle = document.createElement("div");
+  modalTitle.innerText = "Cart";
+  modalTitle.className = "modal-title";
+  modalHeader.append(modalTitle);
+
+  modalContainer.append(modalHeader);
+  // fin ejercicio victoria zaccaro
+
