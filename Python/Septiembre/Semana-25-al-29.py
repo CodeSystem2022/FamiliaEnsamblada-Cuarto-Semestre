335fea28b3101d97603ddecc08f5da27dd24885a
gfduck <66056030+gfduck@users.noreply.github.com>
2023-09-30 17:36:43 -0300

    Gerardo | Python | 25 al 29

--- Python/Septiembre/Semana-25-al-29.py
+++ Python/Septiembre/Semana-25-al-29.py
@@ -35,3 +35,33 @@ print(mensaje)
 
 print(nombre, edad, sueldo, sep=', ')
 // Fin Ejercicio Jesús Mercado
+
+//Ejercicio Gerardo Duckwitz
+  
+# dar formato a un string
+nombre  ='Ariel'
+edad = 28
+mensaje_con_formato = 'Mi nombre es %s y tengo %d años'%(nombre, edad)
+
+#Creamos una tupla
+persona = ('Carlos', 'Gomez', 5000.00)
+mensaje_con_formato = 'Hola %s %s, tu sueldo es %.2f' #% persona # Aqui le pasamos el objeto que es la tupla
+
+#print(mensaje_con_formato  % persona)
+
+nombre = 'Juan'
+edad = 19
+sueldo = 3000
+#mensaje_con_formato = 'Nombre {} Edad {} Sueldo {:2f}'.format(nombre, edad, sueldo)
+mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+#print(mensaje)
+mensaje = 'Nombre {n} Edad {e} Sueldo {s:.2f}'.format(n=nombre, e=edad, s=sueldo)
+
+#print(mensaje)
+
+diccionario = {'nombre': 'Ivan', 'edad' : 35, 'sueldo':8000.00}
+
+mensaje = 'Nombre {persona[nombre]} Edad{persona[edad]} Sueldo{persona[sueldo]:.2f}'.format(persona=diccionario)
+print(mensaje)
+
+//Fin ejercicio Gerardo Duckwitz
