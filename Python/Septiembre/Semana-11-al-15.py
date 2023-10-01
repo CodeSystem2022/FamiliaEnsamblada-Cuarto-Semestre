5adaad71e4af3e3cc6c31f9c6e665fc94f46a740
Matías <69920714+MatiasVilla91@users.noreply.github.com>
2023-10-01 01:04:29 -0300

    Update Semana-11-al-15.py

--- Python/Septiembre/Semana-11-al-15.py
+++ Python/Septiembre/Semana-11-al-15.py
@@ -78,3 +78,72 @@ print(f'Lista de cursos: {lista_cursos}')
 print(len(lista_cursos))
 
 # Fin Ejercicio Jesús Mercado
+
+#Ejercicio Matías Villa
+class MiClase:
+    '''
+    Este es un ejemplo de la documentación de esta clase
+    es solo para saber que aqui esta mi clase
+    '''
+
+    def __init__(self):
+        '''
+        Aqui esta el metodo dunder init
+        Este método es el inicio
+        para esta clase
+        '''
+
+    def mi_metodo(self, param1, param2):
+        '''
+        Este es el método creado por mí que recibe parametros
+        Detecta parametros automaticamente
+        :param param1: Este es el parametro número 1
+        :param param2: Este es el parametro número 2
+        :return: Este es el valor de retorno
+        '''
+
+#Una vez que una variable recibe una cadena, no se puede modificar
+
+#help(str.capitalize)
+mensaje1='hola mundo'
+mensaje2=mensaje1.capitalize()
+print(f'Mensaje1: {mensaje1}, id: {id(mensaje1)}')
+print(f'mensaje2: {mensaje2}, id:{id(mensaje2)}')
+
+mensaje1+= ' Adios'
+print(f'Mensaje1: {mensaje1}, id: {id(mensaje1)}')
+
+#El método split regresa una lista de palabras encontradas en un string y utiliza un delimitador
+
+#help(str.split)
+cursos='Java Javascript Node Python Disenio'
+listar_cursos=cursos.split()
+print(f'Lista de cursos:{listar_cursos}')
+print(type(listar_cursos))
+
+cursos_separados_coma='Java,javascript,node,python,disenio,spring'
+listar_cursos=cursos_separados_coma.split(',',2)
+print(f'Lista de cursos:{listar_cursos}')
+print(len(listar_cursos))
+
+help(str.join)
+
+tupla_str=('Hola','Mundo','Tecnicatura','Universitaria')
+mensaje = ''.join(tupla_str)
+print(f'Mensaje: {mensaje}')
+
+lista_cursos = ['Java','Python','Angular','Spring']
+mensaje=', '.join(lista_cursos)
+print(f'Mensaje:{mensaje}')
+
+cadena='HolaMundo'
+mensaje='.'.join(cadena)
+print(f'Mensaje:{mensaje}')
+
+diccionario= {'nombre':'Juan', 'apellido': 'Perez', 'edad': '18'}
+llaves = '-'.join(diccionario.keys())
+valores = '-'.join(diccionario.values())
+print(f'llaves:{llaves},type:{type(llaves)}')
+print(f'Valores: {valores},Type:{type(valores)}')
+
+#fin Ejercicio Matías Villa. 
