028a0b7bb9b177210040efbae469109839474304
VictoriaZaccaro <104931395+VictoriaZaccaro@users.noreply.github.com>
2023-10-11 00:36:30 -0300

    victoria zaccaro

--- Java/Septiembre/Semana-25-al-29.java
+++ Java/Septiembre/Semana-25-al-29.java
@@ -657,3 +657,108 @@ public class LibroFrom extends JFrame {
     }
 }
 //Fin Matías Villa
+
+//Ejercicio victoria zaccaro
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer idlibro;
+    String nombreLibro;
+    String autor;
+    Double precio;
+    Integer existencias;
+}
+
+package utn.tienda_libros.repositorio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import utn.tienda_libros.modelo.Libro;
+
+public interface LibroRepositorio extends JpaRepository<Libro, Integer> {
+
+}
+
+package utn.tienda_libros.servicio;
+
+import utn.tienda_libros.modelo.Libro;
+
+import java.util.List;
+
+public interface ILibroServicio {
+    public List<Libro> listarLibros();
+    public Libro buscarLibroPorId(Integer idLibro);
+    public void guardarLibro(Libro libro);
+    public void eliminarLibro(Libro libro);
+}
+
+package utn.tienda_libros.servicio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import utn.tienda_libros.modelo.Libro;
+import utn.tienda_libros.repositorio.LibroRepositorio;
+
+import java.util.List;
+
+@Service
+public class LibroServicio implements ILibroServicio{
+    @Autowired
+    private LibroRepositorio libroRepositorio;
+    @Override
+    public List<Libro> listarLibros() {
+        return libroRepositorio.findAll();
+    }
+
+    @Override
+    public Libro buscarLibroPorId(Integer idLibro) {
+        Libro libro = libroRepositorio.findById(idLibro).orElse(null);
+        return libro;
+    }
+
+    @Override
+    public void guardarLibro(Libro libro) {
+    libroRepositorio.save(libro);
+    }
+
+    @Override
+    public void eliminarLibro(Libro libro) {
+    libroRepositorio.delete(libro);
+    }
+}
+import org.springframework.beans.factory.annotation.Autowired;
+import utn.tienda_libros.servicio.LibroServicio;
+
+import javax.swing.*;
+
+public class LibroFrom extends JFrame {
+    LibroServicio libroServicio;
+    private JPanel panel;
+
+    @Autowired
+    public LibroFrom(LibroServicio libroServicio){
+        this.libroServicio = libroServicio;
+        iniciarForma();
+    }
+    private void iniciarForma(){
+        setContentPane(panel);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(900,700);
+    }
+}
+//fin ejercicio victoe¿ria zaccaro
