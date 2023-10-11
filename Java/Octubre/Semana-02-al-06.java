ada2b3d1b016557562f40e227d7dcb3fd206f0c9
alesciAnabel <anabelalesci24@gmail.com>
2023-10-11 17:30:21 -0300

    ejercicio Anabel Alesci Octibre semana 2 al 6

--- Java/Octubre/Semana-02-al-06.java
+++ Java/Octubre/Semana-02-al-06.java
@@ -135,3 +135,39 @@ public class TiendaLibrosApplication{
 }
 
 //Fin Ejercicio Jose Remaggi
+
+//Ejercicio de Anabel Alesci
+//TiendaLibrosApplication.java
+
+package utn.tienda_libros;
+
+import org.springframework.boot.SpringApplication; 
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
+import org.springframework.context.ConfigurableApplicationContext;  
+import org.springframework.boot.builder.SpringApplicationBuilder; 
+import org.springframework.boot.WebApplicationType; 
+import utn.tienda_libros.vista.LibroFrom;
+import java.awt.EventQueue;
+
+@SpringBootApplication
+public class TiendaLibrosApplication{
+	
+	public static void main(String[] args){ 
+		
+		ConfigurableApplicationContext contextoSpring = 
+			new SpringApplicationBuilder(TiendaLibrosApplication.class)
+				.headless(false)
+				.web(WebApplicationType.NONE)
+				.run(args);
+		
+		//Ejecutammos el código para cargar el formulario
+		EventQueue.invokeLater(() -> {   //Método Lambda
+			//Obtenemos el objeto from a través del spring
+			LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
+			libroFrom.setVisible(true);
+
+                });
+    }
+}
+
+//Fin Ejercicio Anabel Alesci
