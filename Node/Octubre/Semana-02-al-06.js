621f6ba82ae8ee6060470dd373c8358557c736d4
Jose Remaggi <joseluisremaggi@gmail.com>
2023-10-11 17:22:54 -0300

    ejercicio semana 2 al 6 octubre

--- Node/Octubre/Semana-02-al-06.js
+++ Node/Octubre/Semana-02-al-06.js
@@ -22,3 +22,27 @@ hola("Carlos", function (nombre) {
 // adios("Carlos", function () {});
 
 //Fin ejercicio Gerardo Duckwitz
+// Ejercicio Jose Remaggi
+function hola(nombre, miCallback) {
+  setTimeout(function () {
+    console.log("Hola " + nombre);
+
+    miCallback(nombre);
+  }, 1000);
+}
+function adios(nombre, otroCallback) {
+  setTimeout(function () {
+    console.log("Adios " + nombre);
+    otroCallback();
+  }, 1000);
+}
+console.log("Iniciando proceso...");
+hola("Carlos", function (nombre) {
+  adios(nombre, function () {
+    console.log("Terminando proceso...");
+  });
+});
+// hola("Carlos", function () {});
+// adios("Carlos", function () {});
+
+//Fin ejercicio Jose Remaggi
