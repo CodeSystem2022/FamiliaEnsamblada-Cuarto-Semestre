1379af94210112ceaf822c47eeef913d0f7f3f6f
gfduck <66056030+gfduck@users.noreply.github.com>
2023-10-17 13:27:51 -0300

    Gerardo | Node | Semana 09 al 12 Octubre

---
+++ Node/Octubre/Semana-09-al-13.js
@@ -0,0 +1,57 @@
+//Ejercicio Gerardo Duckwitz
+function hola(nombre, miCallback) {
+  setTimeout(function () {
+    console.log("Hola " + nombre);
+
+    miCallback(nombre);
+  }, 1000);
+}
+
+function hablar(callbackHablar) {
+  setTimeout(function () {
+    console.log("Bla bla bla bla...");
+    callbackHablar();
+  }, 1000);
+}
+function adios(nombre, otroCallback) {
+  setTimeout(function () {
+    console.log("Adios " + nombre);
+    otroCallback();
+  }, 1000);
+}
+
+function conversacion(nombre, veces, callback) {
+  if (veces > 0) {
+    hablar(function () {
+      conversacion(nombre, --veces, callback);
+    });
+  } else {
+    callback(nombre, callback);
+  }
+}
+//   --Proceso principal
+console.log("Iniciando proceso...");
+hola("Ariel", function (nombre) {
+  conversacion(nombre, 4, function () {
+    console.log("Proceso terminado");
+  });
+});
+
+// hola("Carlos", function (nombre) {
+//   hablar(function () {
+//     hablar(function () {
+//       hablar(function () {
+//         hablar(function () {
+//           adios(nombre, function () {
+//             console.log("Terminando proceso...");
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
+// hola("Carlos", function () {});
+// adios("Carlos", function () {});
+
+
+//Fin ejercicio Gerardo Duckwitz
