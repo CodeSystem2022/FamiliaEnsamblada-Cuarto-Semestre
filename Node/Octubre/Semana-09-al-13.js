af071cd1c31d2384e4b6f92f716afd21cf9b05c1
Yesica López <112655115+Yesica-B-Lopez@users.noreply.github.com>
2023-10-20 15:48:27 -0300

    Yesica Semana-09-al-13.js

--- Node/Octubre/Semana-09-al-13.js
+++ Node/Octubre/Semana-09-al-13.js
@@ -55,3 +55,61 @@ hola("Ariel", function (nombre) {
 
 
 //Fin ejercicio Gerardo Duckwitz
+
+//Ejercicio de Yesica López
+
+function hola(nombre, miCallback){
+    setTimeout(function(){
+      console.log('Hola ' +nombre);
+      miCallback(nombre);
+    }, 1000);
+}
+
+function hablar(callbackHablar){
+    setTimeout(function(){
+      console.log('bla bla bla bla');
+      callbackHablar();
+    }, 1000);
+}
+
+function adios(nombre, otroCallback) {
+    setTimeout(function(){
+      console.log('Adios '+nombre);
+      otroCallback();
+    }, 1600);
+}
+
+//Función Recursiva
+function conversacion(nombre, veces, callback){
+  if (veces > 0){
+    hablar(function(){
+      conversacion(nombre, --veces, callback);
+    });
+  } else {
+    callback(nombre, callback);
+  }
+}
+
+//--Proceso principal
+console.log('Iniciando Proceso...');
+hola('Ariel', function(nombre){
+   conversacion(nombre, 4, function(){    
+    console.log('terminando el proceso...');      
+  });
+});
+
+// hola('Carlos', function(nombre){
+//   hablar(function(){
+//     hablar(function(){
+//        hablar(function(){
+//          hablar(function(){
+//            adios(nombre, function() {
+//             console.log('Terminando proceso...);
+//           });
+//         });
+//       });
+//     });
+//   });
+// });
+
+//Fin Ejercicio Yesica López
