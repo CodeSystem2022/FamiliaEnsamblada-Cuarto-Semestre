9d1849ad33abd66401044f5fc4f2bfe185ee9f94
alesciAnabel <anabelalesci24@gmail.com>
2023-10-20 20:12:36 -0300

    ejercicio semana 9 al 13 octubre alesci

---
+++ Node/Octubre/Semana-09-al-13.js
@@ -0,0 +1,231 @@
+//Ejercicio Gerardo Duckwitz
+function hola(nombre, miCallback) {
+    setTimeout(function () {
+      console.log("Hola " + nombre);
+  
+      miCallback(nombre);
+    }, 1000);
+  }
+  
+  function hablar(callbackHablar) {
+    setTimeout(function () {
+      console.log("Bla bla bla bla...");
+      callbackHablar();
+    }, 1000);
+  }
+  function adios(nombre, otroCallback) {
+    setTimeout(function () {
+      console.log("Adios " + nombre);
+      otroCallback();
+    }, 1000);
+  }
+  
+  function conversacion(nombre, veces, callback) {
+    if (veces > 0) {
+      hablar(function () {
+        conversacion(nombre, --veces, callback);
+      });
+    } else {
+      callback(nombre, callback);
+    }
+  }
+  //   --Proceso principal
+  console.log("Iniciando proceso...");
+  hola("Ariel", function (nombre) {
+    conversacion(nombre, 4, function () {
+      console.log("Proceso terminado");
+    });
+  });
+  
+  // hola("Carlos", function (nombre) {
+  //   hablar(function () {
+  //     hablar(function () {
+  //       hablar(function () {
+  //         hablar(function () {
+  //           adios(nombre, function () {
+  //             console.log("Terminando proceso...");
+  //           });
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
+  // hola("Carlos", function () {});
+  // adios("Carlos", function () {});
+  
+  
+  //Fin ejercicio Gerardo Duckwitz
+  
+  //Ejercicio de Yesica López
+  
+  function hola(nombre, miCallback){
+      setTimeout(function(){
+        console.log('Hola ' +nombre);
+        miCallback(nombre);
+      }, 1000);
+  }
+  
+  function hablar(callbackHablar){
+      setTimeout(function(){
+        console.log('bla bla bla bla');
+        callbackHablar();
+      }, 1000);
+  }
+  
+  function adios(nombre, otroCallback) {
+      setTimeout(function(){
+        console.log('Adios '+nombre);
+        otroCallback();
+      }, 1600);
+  }
+  
+  //Función Recursiva
+  function conversacion(nombre, veces, callback){
+    if (veces > 0){
+      hablar(function(){
+        conversacion(nombre, --veces, callback);
+      });
+    } else {
+      callback(nombre, callback);
+    }
+  }
+  
+  //--Proceso principal
+  console.log('Iniciando Proceso...');
+  hola('Ariel', function(nombre){
+     conversacion(nombre, 4, function(){    
+      console.log('terminando el proceso...');      
+    });
+  });
+  
+  // hola('Carlos', function(nombre){
+  //   hablar(function(){
+  //     hablar(function(){
+  //        hablar(function(){
+  //          hablar(function(){
+  //            adios(nombre, function() {
+  //             console.log('Terminando proceso...);
+  //           });
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
+  
+  //Fin Ejercicio Yesica López
+  
+  //Ejercicio de Jose Remaggi
+  
+  function hola(nombre, miCallback){
+    setTimeout(function(){
+      console.log('Hola ' +nombre);
+      miCallback(nombre);
+    }, 1000);
+  }
+  
+  function hablar(callbackHablar){
+    setTimeout(function(){
+      console.log('bla bla bla bla');
+      callbackHablar();
+    }, 1000);
+  }
+  
+  function adios(nombre, otroCallback) {
+    setTimeout(function(){
+      console.log('Adios '+nombre);
+      otroCallback();
+    }, 1600);
+  }
+  
+  //Función Recursiva
+  function conversacion(nombre, veces, callback){
+  if (veces > 0){
+    hablar(function(){
+      conversacion(nombre, --veces, callback);
+    });
+  } else {
+    callback(nombre, callback);
+  }
+  }
+  
+  //--Proceso principal
+  console.log('Iniciando Proceso...');
+  hola('Ariel', function(nombre){
+   conversacion(nombre, 4, function(){    
+    console.log('terminando el proceso...');      
+  });
+  });
+  
+  // hola('Carlos', function(nombre){
+  //   hablar(function(){
+  //     hablar(function(){
+  //        hablar(function(){
+  //          hablar(function(){
+  //            adios(nombre, function() {
+  //             console.log('Terminando proceso...);
+  //           });
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
+  
+  //Fin Ejercicio Jose Remaggi
+  
+//Ejercicio de Anabel Alesci
+  
+    function hola(nombre, miCallback){
+        setTimeout(function(){
+          console.log('Hola ' +nombre);
+          miCallback(nombre);
+        }, 1000);
+      }
+      
+      function hablar(callbackHablar){
+        setTimeout(function(){
+          console.log('bla bla bla bla');
+          callbackHablar();
+        }, 1000);
+      }
+      
+      function adios(nombre, otroCallback) {
+        setTimeout(function(){
+          console.log('Adios '+nombre);
+          otroCallback();
+        }, 1600);
+      }
+      
+      //Función Recursiva
+      function conversacion(nombre, veces, callback){
+      if (veces > 0){
+        hablar(function(){
+          conversacion(nombre, --veces, callback);
+        });
+      } else {
+        callback(nombre, callback);
+      }
+      }
+      
+      //--Proceso principal
+      console.log('Iniciando Proceso...');
+      hola('Ariel', function(nombre){
+       conversacion(nombre, 4, function(){    
+        console.log('terminando el proceso...');      
+      });
+      });
+      
+      // hola('Carlos', function(nombre){
+      //   hablar(function(){
+      //     hablar(function(){
+      //        hablar(function(){
+      //          hablar(function(){
+      //            adios(nombre, function() {
+      //             console.log('Terminando proceso...);
+      //           });
+      //         });
+      //       });
+      //     });
+      //   });
+      // });
+      
+      //Fin Ejercicio Anabel Alesci
