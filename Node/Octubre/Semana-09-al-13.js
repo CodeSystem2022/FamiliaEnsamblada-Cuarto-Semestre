313ddb277444070093db07b6c9e1b40d805fab3a
VictoriaZaccaro <104931395+VictoriaZaccaro@users.noreply.github.com>
2023-10-23 18:39:41 -0300

    victoria zaccaro

--- Node/Octubre/Semana-09-al-13.js
+++ Node/Octubre/Semana-09-al-13.js
@@ -230,3 +230,59 @@ function hola(nombre, miCallback) {
       // });
       
       //Fin Ejercicio Anabel Alesci
+
+//Ejercicio victoria zaccaro
+ function hola(nombre, miCallback){
+      setTimeout(function(){
+        console.log('Hola ' +nombre);
+        miCallback(nombre);
+      }, 1000);
+  }
+  
+  function hablar(callbackHablar){
+      setTimeout(function(){
+        console.log('bla bla bla bla');
+        callbackHablar();
+      }, 1000);
+  }
+  
+  function adios(nombre, otroCallback) {
+      setTimeout(function(){
+        console.log('Adios '+nombre);
+        otroCallback();
+      }, 1600);
+  }
+  
+  //Función Recursiva
+  function conversacion(nombre, veces, callback){
+    if (veces > 0){
+      hablar(function(){
+        conversacion(nombre, --veces, callback);
+      });
+    } else {
+      callback(nombre, callback);
+    }
+  }
+  
+  //--Proceso principal
+  console.log('Iniciando Proceso...');
+  hola('Ariel', function(nombre){
+     conversacion(nombre, 4, function(){    
+      console.log('terminando el proceso...');      
+    });
+  });
+  
+  // hola('Carlos', function(nombre){
+  //   hablar(function(){
+  //     hablar(function(){
+  //        hablar(function(){
+  //          hablar(function(){
+  //            adios(nombre, function() {
+  //             console.log('Terminando proceso...);
+  //           });
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
+//Fin ejercicio victoria zaccaro
