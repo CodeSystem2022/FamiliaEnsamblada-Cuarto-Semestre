98aec4c42a0b73c23cbb5bd3e0c22f67b81dba1e
Matías <69920714+MatiasVilla91@users.noreply.github.com>
2023-10-28 19:18:32 -0300

    Update Semana-16-al-20.js

--- Node/Octubre/Semana-16-al-20.js
+++ Node/Octubre/Semana-16-al-20.js
@@ -136,3 +136,84 @@ hola('Ariel')
     })
 
 // Fin Ejercicio Jesús Mercado
+//Matías Villa
+function soyAsincrona(nombre) {
+    return new Promise(function(resolve, reject ){
+        
+    setTimeout(function() {
+        console.log('Hola : '+nombre)
+        resolve(nombre);
+    }, 1000);
+    }); 
+   
+}
+
+function hablar(nombre){
+    
+        return new Promise((resolve, reject) =>{ //Usamos sintaxis ES6
+        setTimeout(function(){
+        console.log('bla bla bla');
+        resolve(nombre);
+    }, 1000);
+})}
+
+function adios(nombre){
+        return new Promise((resolve, reject)=>{
+        setTimeout(function(){
+            console.log('Adios '+ nombre);
+            //resolve();
+            reject('Hay un error')
+        }, 1000);
+   
+
+})}
+
+//Llamos al a función
+
+console.log('Iniciando la revolución');
+soyAsincrona('Ariel')
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(adios)
+    .then((nombre) => {
+        console.log('Terminando la revolución');
+    })
+    .catch(error=>{
+        console.log('Ha habido un error:');
+        console.log(error)
+    })
+
+
+
+//función recursiva
+function conversacion(nombre, veces, callback){
+    if(veces > 0){
+    hablar(function(){
+          conversacion(nombre, --veces, callback);  
+    });}
+    else{
+        callback(nombre, callback);
+    }
+}
+//--Proceso principal--
+console.log('Iniciando...')
+soyAsincrona('Async', function(nombre){
+    conversacion(nombre, 10 , function(){
+        console.log('Terminando ')
+    });
+    
+//hablar(function(){ 
+// hablar(function(){   
+//  hablar(function(){
+//    hablar(function(){ 
+//        adios(nombre, function(){
+//            console.log('Terminando')
+//        });
+//    });
+//    });
+ //   });
+//}); 
+});
+
+//Fin Matías Villa
