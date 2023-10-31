bd1cfca9325cdaedfd11a6a9cf191244261b9945
VictoriaZaccaro <104931395+VictoriaZaccaro@users.noreply.github.com>
2023-10-30 21:24:00 -0300

    victoria zaccaro

--- Node/Octubre/Semana-16-al-20.js
+++ Node/Octubre/Semana-16-al-20.js
@@ -136,6 +136,48 @@ hola('Ariel')
     })
 
 // Fin Ejercicio Jesús Mercado
+//Ejercicio victoria zaccaro
+function hola(nombre) {
+    return new Promise( function( resolve, reject) {
+      setTimeout( function() {
+        console.log('Hola ' + nombre);
+        resolve(nombre);
+      }, 1000);
+    });
+  }
+  function hablar(nombre){
+     return new Promise( (resolve, reject) => { //usamos la sintaxis ES6
+       setTimeout( function() {
+         console.log('bla bla bla bla...');
+         resolve(nombre);
+       }, 1000);
+     });
+  }
+  function adios(nombre){
+     return new Promise( (resolve, reject) => {   
+       setTimeout(function() {
+         console.log('Adios ' +nombre);
+         //resolve();
+        reject('Hay un error');
+      }, 1000);
+    });
+  }
+
+//Llamamos a la función
+  console.log('Iniciando el proceso...');
+  hola('Ariel')
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(adios)                                           
+    .then((nombre) => {
+      console.log('Terminado el proceso');
+    })
+    .catch(error => {
+      console.error('Ha habido un error: ');      
+      console.error(error);                        
+    })                                      
+//fin ejercicio victoria zaccaro
 //Matías Villa
 function soyAsincrona(nombre) {
     return new Promise(function(resolve, reject ){
