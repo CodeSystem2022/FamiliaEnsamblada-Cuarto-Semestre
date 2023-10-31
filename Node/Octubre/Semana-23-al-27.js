3cc25c357d36facbc241cd226c6e26f90b0c3594
Jesús Mercado <83405417+mercadojesus@users.noreply.github.com>
2023-10-31 01:01:48 -0300

    Update Semana-23-al-27.js
    Ejercicio de Node correspondiente a la semana del 23/10 al 27/10

--- Node/Octubre/Semana-23-al-27.js
+++ Node/Octubre/Semana-23-al-27.js
@@ -547,3 +547,95 @@ async function conversation(name){
 }
 conversation("Anabel")
 //Fin Ejercicio Anabel Alesci
+
+// Ejercicio Jesús Mercado
+
+// La palabra async no es necesaria en las funciones porque ya son asincronas
+// Igual proyectan una sincronía visual
+async function hola(nombre){
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            console.log('Hola ' + nombre);
+            resolve(nombre);
+        }, 1000);
+    });
+}
+
+async function hablar(nombre){
+    return new Promise((resolve, reject) => { // Usamos la sintaxis ES6
+        setTimeout( function () {
+            console.log('bla bla bla bla');
+            resolve(nombre);
+        }, 1000);
+    });
+}
+
+async function adios(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            // validamos el error o aprobacion
+            console.log('Adios ' + nombre);
+            // if(err) reject('Hay un error');
+            resolve();
+        }, 1000);
+    });
+}
+
+// await hola('Ariel'); // esto es una mala sintaxis
+// await es sólo es válido dentro de una función asíncrona
+async function main(){
+    let nombre = await hola('Ariel');
+    await hablar();
+    await hablar();
+    await hablar();
+    await adios(nombre);
+    console.log('Termina el proceso...');
+}
+
+console.log('Empezamos el proceso...');
+main();
+console.log('Esta va a ser la segunda instrucción');
+
+// Código en inglés
+// Es asíncrono y retorna una promesa
+function sayHello(name){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Hello ' + name);
+            resolve(name);
+        }, 1000);
+    });
+}
+
+function talk(name){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('bla bla bla bla');
+            resolve(name);
+        }, 1000);
+    });
+}
+
+function sayBye(name) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log('Goodbye ' + name);
+            resolve(name);
+        }, 1000);
+    });
+}
+
+async function conversation(name){
+    console.log('Code in english');
+    console.log("Starting async process...");
+    await sayHello(name);
+    await talk();
+    await talk();
+    await talk();
+    await sayBye(name);
+    console.log("Process completed");
+}
+
+conversation("Ariel");
+
+// Fin Ejercicio Jesús Mercado
