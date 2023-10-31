46ba2f8faea45987ffa1de5cc3658ea767983bf6
VictoriaZaccaro <104931395+VictoriaZaccaro@users.noreply.github.com>
2023-10-30 21:32:40 -0300

    victoria zaccaro

--- Node/Octubre/Semana-23-al-27.js
+++ Node/Octubre/Semana-23-al-27.js
@@ -80,6 +80,95 @@ main2();
 
 //Fin ejercicio Gerardo Duckwitz
 
+//Ejercicio Victoria zaccaro
+//La palabra async no es necesario en las funciones, porque ya son asincronas
+//Igual proyectan una asincronia visual
+async function hola(nombre) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      console.log('Hola '+ nombre);
+      resolve(nombre);
+    }, 1000);
+  });
+}
+
+async function hablar(nombre) {
+  return new Promise( (resolve, reject) => { //usamos la sintaxis ES6
+    setTimeout(function() {
+      console.log('bla bla bla bla');
+      resolve(nombre);
+    }, 1000);
+  });
+}
+async function adios(nombre) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      //validamos el error o aprobación
+      console.log('Adios '+ nombre);
+      //if(err) reject('Hay un error');
+       resolve();
+    }, 1000);
+  });
+}
+
+// await hola('Ariel'); // Esto es una mala sintaxis
+//await solo es válido dentro de una función asincrona  
+async function main() {
+  let nombre = await hola('Ariel');
+  await hablar();
+  await hablar();
+  await hablar();
+  await adios(nombre);
+  console.log('Termina el proceso...'); 
+}
+//console.log('Empezamos el proceso...');
+//main();
+//console.log('Esta va a ser la segunda instrucción');
+
+
+//Código en Inglés
+//Es asincrono y retorna una promesa
+function sayHello(name) {   
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Hello '+ nombre);
+      resolve(name);
+    }, 1000);
+  });
+}
+
+function talk(name) {     
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('bla bla bla bla');
+      resolve(name);
+    }, 1000);
+  });
+}
+
+function sayBye(name) {  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+       console.log('Goodbye '+name);
+       resolve(name);
+    }, 1000);
+  });
+}
+
+async function conversation(name) {
+  console.log('Code in english');
+  console.log('Starting async process...');
+  await sayHello(name);
+  await talk();
+  await talk();
+  await talk();
+  await sayBye(name);
+  console.log('Process completed');
+}
+
+conversation('Ariel');
+//Fin Ejercicio Victoria Zaccaro
+
 //Ejercicio Matías Villa
 //La palabra async no son necesarias porque las funciones ya son asincronas
 //igual proyectan una sincronia visual 
