25ffc56b8bc380e59aa40990f7939d39ee1dcec9
Jesús Mercado <83405417+mercadojesus@users.noreply.github.com>
2023-11-05 19:29:59 -0300

    Update Semana-01-al-03.py
    Ejercicio Python correspondiente a la semana del 30/10 al 03/11

--- Python/Noviembre/Semana-01-al-03.py
+++ Python/Noviembre/Semana-01-al-03.py
@@ -22,3 +22,27 @@ if __name__ =="__main__"
 
 
 # Fin ejercicio Gerardo Duckwitz
+
+// Ejercicio Jesús Mercado
+
+import matplotlib.pyplot as plt
+
+def generate_pie_chart():
+    labels = ['A', 'B', 'C']
+    values = [200,34,120]
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    plt.savefig('pie.png')
+
+    plt.close()
+
+# archivo main
+import charts
+
+def run():
+    charts.generate_pie_chart()
+
+if __name__ =="__main__"
+    run()
+
+// Fin Ejercicio Jesús Mercado
