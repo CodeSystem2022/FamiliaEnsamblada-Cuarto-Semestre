b743e9c4d179c7939a17f660e338a9e4bf2625df
Jesús Mercado <83405417+mercadojesus@users.noreply.github.com>
2023-11-05 19:40:42 -0300

    Update Semana-01-al-03.js
    Ejercicio Node correspondiente a la semana del 30/10 al 03/11

--- Node/Noviembre/Semana-01-al-03.js
+++ Node/Noviembre/Semana-01-al-03.js
@@ -79,3 +79,47 @@ setImmediate(() => {
 global.miVariable = "mi variable globnal";
 console.log(miVariable);
 //Fin ejercicio Gerardo Duckwitz
+
+// Ejercicio Jesús Mercado
+
+//this===globlar=true
+//Los modulos le permite a node tener funcionalidades 
+
+//Mostrar algo en consola
+//console.log();
+//mostrar mensaje en forma de error
+//console.error();
+//Ejecutar código despues de un intervalo de tiempo
+//setTimeout(()=>{});
+//Ejecutar código cada intervalo de tiempo
+//setInterval(()=>{});
+//Da prioridad a la ejecución de una funcion asincrona
+//setImmediate(()=>{});
+
+
+//console.log(setInterval);
+
+//let i = 0;
+//let intervalo=setInterval(()=>{
+//    console.log('Hola')
+//    if(i ==3){
+//        clearInterval(intervalo); //Detenemos la función
+//    }
+//    i++
+//}, 1000);
+
+setImmediate(()=>{
+    console.log('Saludo Inmediato');
+
+})
+
+//require();
+
+console.log(process);
+console.log(__dirname); //Muestra directorio donde trabajamos
+console.log(__filename);
+
+global.miVariable = 'mi variable Global';
+console.log(global.miVariable);
+
+// Fin Ejercicio Jesús Mercado
