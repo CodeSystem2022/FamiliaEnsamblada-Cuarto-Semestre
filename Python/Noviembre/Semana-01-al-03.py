b4d67a64613e4a305ddd2a3f3afbe0c5c8df622a
Yesica López <112655115+Yesica-B-Lopez@users.noreply.github.com>
2023-11-06 10:23:37 -0300

    Yesica Semana-01-al-03.py

--- Python/Noviembre/Semana-01-al-03.py
+++ Python/Noviembre/Semana-01-al-03.py
@@ -46,3 +46,29 @@ if __name__ =="__main__"
     run()
 
 // Fin Ejercicio Jesús Mercado
+
+#Ejercicio de Yesica López
+
+#charts.py
+
+import matplotlib.pyplot as plt
+
+def generate_pie_chart():
+    labels = ['A', 'B', 'C']
+    values = [200,34,120]
+    fig, ax = plt.subplots()
+    ax.pie(values, labels=labels)
+    plt.savefig('pie.png')
+
+    plt.close()
+
+# main.py
+import charts
+
+def run():
+    charts.generate_pie_chart()
+
+if __name__ =='__main__':
+    run()
+
+#Fin Ejercicio Yesica López
