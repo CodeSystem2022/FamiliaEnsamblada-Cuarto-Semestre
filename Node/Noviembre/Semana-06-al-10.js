04df4f658ed1ab718a72822097fe3ed69b3ea3cb
Jesús Mercado <83405417+mercadojesus@users.noreply.github.com>
2023-11-12 11:28:38 -0300

    Update Semana-06-al-10.js
    Ejercicio Node correspondiente a la semana del 06/11 al 10/11

--- Node/Noviembre/Semana-06-al-10.js
+++ Node/Noviembre/Semana-06-al-10.js
@@ -29,3 +29,36 @@ function borrar(ruta, cb) {
 borrar(__dirname + "/archivo1.txt", console.log);
 
 //Fin ejercicio Gerardo Duckwitz
+
+// Ejercicio Jesús Mercado
+
+const fs = require("fs");
+
+// Primero leemos el archivo.txt
+function leer(ruta, cb) {
+  fs.readFile(ruta, (err, data) => {
+    // Leido el archivo
+    cb(data.toString());
+  });
+}
+// leer(__dirname + "/archivo.txt", console.log);
+
+//Segundo escribimos el archivo1.txt creandolo
+function escribir(ruta, contenido, cb) {
+  fs.writeFile(ruta, contenido, function (err) {
+    if (err) {
+      console.error("No he podido escribirlo", err);
+    } else {
+      console.log("Se ha escrito correctamente");
+    }
+  });
+}
+escribir(__dirname + "/archivo1.txt", "Reescribimos el archivo", console.log);
+
+//Tercero eliminamos el archivo1.txt
+function borrar(ruta, cb) {
+  fs.unlink(ruta, cb);
+}
+borrar(__dirname + "/archivo1.txt", console.log);
+
+// Fin Ejercicio Jesús Mercado
