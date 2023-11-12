98403aebce42778b76d76d077c38fe51308d530c
Yesica López <112655115+Yesica-B-Lopez@users.noreply.github.com>
2023-11-12 19:27:21 -0300

    Yesica Semana-06-al-10.js

--- Node/Noviembre/Semana-06-al-10.js
+++ Node/Noviembre/Semana-06-al-10.js
@@ -167,3 +167,37 @@ function borrar(ruta,cb){
 borrar(`${__dirname}/archivo.txt`,console.log);
 
 //Fin ejercicio Matías
+
+//Ejercicio de Yesica López
+//fs.js
+const fs = require('fs');
+
+// Primero leemos el archivo.txt
+function leer(ruta, cb) {
+  fs.readFile(ruta, (err, data) => {
+    cb(data.toString());
+  })
+}
+// leer(`${__dirname}/archivo.txt`, console.log); //Sintaxis ES6
+
+//Segundo escribimos el archivo1.txt creandolo
+function escribir(ruta, contenido, cb) {
+  fs.writeFile(ruta, contenido, function (err){
+    if (err) {
+      console.log('No he podido escribirlo', err);
+    } else {
+      console.log('Se ha escrito correctamente');
+    }
+  })
+}
+
+//Tercero eliminamos el archivo1.txt
+function borrar(ruta, cb) {
+  fs.unlink(ruta, cb); //elimina de manera asincrona
+}
+
+borrar(`${__dirname}/archivo1.txt`, console.log);
+escribir(`${__dirname}/archivo1.txt`, 'Reescribimos el archivo', console.log);
+leer(`${__dirname}/archivo1.txt`,console.log);//Sintaxis ES6
+
+//Fin Ejercicio Yesica López
