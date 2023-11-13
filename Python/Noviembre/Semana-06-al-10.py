27c5d4e053c0b24e2d97ead4cc62949d9e3c3ef0
Yesica López <112655115+Yesica-B-Lopez@users.noreply.github.com>
2023-11-12 22:10:05 -0300

    Yesica Semana-06-al-10.py

--- Python/Noviembre/Semana-06-al-10.py
+++ Python/Noviembre/Semana-06-al-10.py
@@ -193,3 +193,53 @@ if __name__ == '__main__':
 
 # Fin Ejercicio Anabel Alesci
 
+#Ejercicio de Yesica López
+import utils
+import read_csv
+import charts
+
+def run():
+  data = read_csv.read_csv('./data.csv') # leemos el csv, gracias al módulo línea 2
+  data = list(filter(lambda item : item['Continent'] == 'South America',data))
+
+  countries = list(map(lambda x: x['Country'], data))
+  percentages = list(map(lambda x: x['World Population Percentage'], data))
+  charts.generate_pie_chart(countries, percentages)
+
+  country = input('Type Country => ')
+  print(country)
+
+  result = utils.population_by_country(data, country)
+
+  if len(result) > 0:
+    country = result[0]
+    print(country)
+    labels, values = utils.get_population(country)
+    charts.generate_bar_chart(country['country'], labels, values)
+
+if __name__ == '__main__':
+  run()
+
+
+import matplotlib.pyplot as plt
+
+def generate_bar_chart(name, labels, values):
+  fig, ax = plt.subplots()
+  ax.bar(labels, values)
+  plt.savefig(f'./img/{name}.png')
+  plt.close()
+
+def generate_pie_chart(labels, values):
+  fig, ax = plt.subplots()
+  ax.pie(values, labels=labels)
+  ax.axis('equal')
+  plt.savefig('bar.png')
+  plt.close()
+  
+if __name__ == '__main__':
+  labels = ['a', 'b', 'c']
+  values = [10, 40, 800]
+  # generate_bar_chart(labels, values)
+  generate_pie_chart(labels, values)
+
+#Fin Ejercicio Yesica López
